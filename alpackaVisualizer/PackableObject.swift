be98921ddeef85f10b1788ffdc8e88676ff0b5d7
Ethan John <ejjonny@gmail.com>
2020-01-02 22:35:25 -0800

    Packing rect

---
+++ alpackaVisualizer/PackableObject.swift
@@ -0,0 +1,15 @@
+//
+//  PackableObject.swift
+//  alpackaVisualizer
+//
+//  Created by Ethan John on 1/2/20.
+//  Copyright © 2020 Ethan John. All rights reserved.
+//
+
+import UIKit
+
+struct PackableObject {
+    let origin: CGPoint
+    let width: CGFloat
+    let height: CGFloat
+}
---
+++ alpackaVisualizer/PackingRect.swift
@@ -0,0 +1,39 @@
+//
+//  PackingRect.swift
+//  alpackaVisualizer
+//
+//  Created by Ethan John on 1/2/20.
+//  Copyright © 2020 Ethan John. All rights reserved.
+//
+
+import SwiftUI
+
+struct PackingRect: View {
+    @State var objects: [PackableObject] = []
+    var body: some View {
+        GeometryReader { proxy in
+            ForEach(self.objects.indices, id: \.self) { index in
+                return Rectangle()
+                    .offset(self.objects[index].origin)
+                    .frame(width: self.objects[index].width, height: self.objects[index].height)
+                    .foregroundColor(self.randomColor())
+                    .transition(.scale)
+            }
+        }
+    }
+    
+    func randomColor() -> Color {
+        Color(red: Double.random(in: 0...255) / 255,
+              green: Double.random(in: 0...255) / 255,
+              blue: Double.random(in: 0...255) / 255)
+    }
+}
+
+struct PackingRect_Previews: PreviewProvider {
+    static var previews: some View {
+        PackingRect(objects: [
+            PackableObject(origin: CGPoint(x: 20, y: 20), width: 20, height: 20),
+            PackableObject(origin: CGPoint(x: 40, y: 11), width: 134, height: 40)
+        ])
+    }
+}
