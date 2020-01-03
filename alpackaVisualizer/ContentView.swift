c9a25aabfbfd60e97dd2661be118a224950c0cc0
Ethan John <ejjonny@gmail.com>
2020-01-02 22:05:11 -0800

    Initial

---
+++ alpackaVisualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alpackaVisualizer
+//
+//  Created by Ethan John on 1/2/20.
+//  Copyright © 2020 Ethan John. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
